975338fb574c360c177ce1743aa21f2351d594de
Shamil Isayev <shamili@wizardsdev.com>
2021-04-21 19:00:38 +0300

    upd

--- src/modules/mainPage/StyledComponent.js
+++ src/modules/mainPage/StyledComponent.js
@@ -1,9 +1,25 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+`;
 export const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+Wrapper.Top = styled.div`
+  width: 100%;
+  height: 50vh;
+`;
+
+Wrapper.Bottom = styled.div`
+  width: 100%;
+  height: 50vh;
 `;
