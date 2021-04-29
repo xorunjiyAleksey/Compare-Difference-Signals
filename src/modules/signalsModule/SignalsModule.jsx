599f70a7154637def1a187a972c3e6999ec37168
Shamil Isayev <shamili@wizardsdev.com>
2021-04-29 09:23:02 +0300

    fix

--- src/modules/signalsModule/SignalsModule.jsx
+++ src/modules/signalsModule/SignalsModule.jsx
@@ -12,21 +12,106 @@ const SignalsModule = props => {
         sendAutoSignal,
         sendSdsSignal
     } = props;
+
     const parsedSdsSignals = getSdsSignals.map(el => JSON.parse(el));
-    console.log('parsedSdsSignals', parsedSdsSignals)
-    console.log('getAutochartistSignals', getAutochartistSignals)
-    // const parsedSdsSignals.map(el => el.id)
-    function isEqual(parsedSdsSignals, getAutochartistSignals) {
-        return parsedSdsSignals.map(function (one) {
-            return getAutochartistSignals.some(function (two) {
-                return two.val === one.val;
-            });
-        }).indexOf(false) > -1 ? false : true;
+
+    console.log('parsedSdsSignals', parsedSdsSignals);
+    console.log('getAutochartistSignals', getAutochartistSignals);
+
+    const sortedSdsSignals = [];
+    const toSortSds = (parsedSdsSignals) => {
+        const toSdsIdArray = [];
+        parsedSdsSignals.map(el => toSdsIdArray.push(el.resultUid));
+        sortedSdsSignals.push(toSdsIdArray.sort());
     }
+    toSortSds(parsedSdsSignals);
+    console.log(sortedSdsSignals)
 
+    const sortedAutoSignals = [];
+    const toSortAuto = (getAutochartistSignals) => {
+        const toAutoIdArray = [];
+        getAutochartistSignals.map(el => toAutoIdArray.push(el.resultUid));
+        getAutochartistSignals.push(toAutoIdArray.sort());
+    }
+    toSortAuto(sortedAutoSignals);
+    console.log(sortedAutoSignals);
 
-    isEqual(parsedSdsSignals, getAutochartistSignals)
+    // let compareResultAuto = [];
+    // let compareResultSds = [];
+    // const isPatternsEqual = (parsedSdsSignals, getAutochartistSignals) => {
+    //     for(let i = 0; i < parsedSdsSignals?.length; i++) {
+    //         for(let j = 0; j < getAutochartistSignals?.length; j++) {
+    //             if(parsedSdsSignals[i].resultUid !== getAutochartistSignals[j].resultUid) {
+    //                 compareResultSds.push(getAutochartistSignals[j])
+    //                 compareResultAuto.push(parsedSdsSignals[i])
+    //             }
+    //         }
+    //     }
+    // }
+    // isPatternsEqual(parsedSdsSignals, getAutochartistSignals);
+    // console.log([...new Set(compareResultAuto)]);
+    // console.log([...new Set(compareResultSds)]);
 
+    // let result = _.differenceBy(parsedSdsSignals, getAutochartistSignals, 'resultUid');
+    // console.log(result)
+
+    // const defaulKeysAuto = {
+    //     age: getAutochartistSignals.age,
+    //     breakout: getAutochartistSignals.breakout,
+    //     clarity: getAutochartistSignals.clarity,
+    //     clickThroughUrl: getAutochartistSignals.clickThroughUrl,
+    //     completed: getAutochartistSignals.completed,
+    //     dataFeed: getAutochartistSignals.dataFeed,
+    //     demoCandleDelay: getAutochartistSignals.demoCandleDelay,
+    //     demoMinuteDelay: getAutochartistSignals.demoCandleDelay,
+    //     direction: getAutochartistSignals.direction,
+    //     exchange: getAutochartistSignals.exchange,
+    //     initialTrend: getAutochartistSignals.initialTrend,
+    //     interval: getAutochartistSignals.interval,
+    //     length: getAutochartistSignals.length,
+    //     pattern: getAutochartistSignals.pattern,
+    //     patternEndTime: getAutochartistSignals.patternEndTime,
+    //     patternImageUrl: getAutochartistSignals.patternImageUrl,
+    //     predictionPriceFrom: getAutochartistSignals.predictionPriceFrom,
+    //     predictionPriceTo: getAutochartistSignals.predictionPriceFrom,
+    //     predictionTimeFrom: getAutochartistSignals.predictionPriceFrom,
+    //     predictionTimeTo: getAutochartistSignals.predictionPriceFrom,
+    //     quality: getAutochartistSignals.quality,
+    //     relevant: getAutochartistSignals.relevant,
+    //     resistanceX0: getAutochartistSignals.resistanceX0,
+    //     resistanceX1: getAutochartistSignals.resistanceX0,
+    //     resistanceY0: getAutochartistSignals.resistanceX0,
+    //     resistanceY1: getAutochartistSignals.resistanceY1,
+    //     resultUid: getAutochartistSignals.resultUid
+    //     stopLoss: getAutochartistSignals.stopLoss,
+    //     supportX0: getAutochartistSignals.resistanceX0,
+    //     supportX1: getAutochartistSignals.supportX1,
+    //     supportY0: getAutochartistSignals.supportY0,
+    //     supportY1: getAutochartistSignals.supportX1,
+    //     symbol: getAutochartistSignals.symbol,
+    //     symbolCode: getAutochartistSignals.symbolCode,
+    //     timezoneOffset: getAutochartistSignals.timezoneOffset,
+    //     trend: getAutochartistSignals.trend,
+    //     uniformity: getAutochartistSignals.uniformity,
+    //     volumeIncrease: getAutochartistSignals.volumeIncrease
+    // }
+
+    //  const isEqual = (parsedSdsSignals, getAutochartistSignals) => {
+    //         return parsedSdsSignals.map(el => {
+    //             return getAutochartistSignals.map(item => {
+    //                 if (item.resultUid !== el.resultUid) {
+    //                     console.log('auth', item.resultUid, 'sds', el.resultUid)
+    //                     return item.resultUid
+    //                 }
+    //             });
+    //         })
+    //     }
+    // console.log(isEqual(parsedSdsSignals, getAutochartistSignals));
+
+    // const arr1IDs = new Set(getAutochartistSignals.map(({ id }) => id));
+    // const combined = [...getAutochartistSignals, ...getAutochartistSignals.filter(({ id }) => !arr1IDs.has(id))];
+    //
+    // console.log(combined)
 
     return (
         <MainWrapper>
